ea4d931abc520b9c3cc74810ff5cb5a16602faa9
Sonyaandr <andreevasd8@gmail.com>
2024-06-16 17:40:24 +0300

    Исправлено

--- src/Main.java
+++ src/Main.java
@@ -18,45 +18,35 @@ public class Main {
     }
 
     public static void isLeapYear(int year) {
-        if (year % 4 != 0) {
-            System.out.println(year + " год - не является високосным");
-        } else if (year % 100 != 0) {
-            System.out.println(year + " год - високосный");
-        } else if (year % 400 != 0) {
-            System.out.println(year + " год - не является високосным");
+        if ((year % 4 != 0) && (year % 100 = 0) && (year % 400 != 0) && (year > 1584)) {
+            System.out.println(year + "год - не является високосный");
         } else {
             System.out.println(year + "год - високосный");
         }
-
     }
 
     public static void application(int clientOS, int yearDevice) {
         int currentYear = 2015;
         if (clientOS == 0 && yearDevice < currentYear) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
         }
         if (clientOS == 1 && yearDevice < currentYear) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        }
-        if (clientOS == 0 && yearDevice >= currentYear) {
+        } else {
             System.out.println("Установите версию приложения для iOS по ссылке");
         }
-        if (clientOS == 1 && yearDevice >= currentYear) {
-            System.out.println("Установите версию приложения для Android по ссылке");
-        }
-
     }
 
     public static int calculateDayDelivery(int distans) {
         int day = 0;
-        if (distans < 21) {
+        if (distans <= 20) {
             day = day + 1;
-        } else if (distans > 20 && distans < 61) {
+        } else if (distans > 20 && distans <= 60) {
             day = day + 2;
-        } else if (distans > 60 && distans < 101) {
+        } else if (distans > 60 && distans <= 100) {
             day = day + 3;
-        } else if (distans > 100) {
-            day = 0;
         }
         return day;
 

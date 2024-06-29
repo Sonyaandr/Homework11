fb2d2dbfe75fcc32687781db05e701f58acd7c2f
Sonyaandr <andreevasd8@gmail.com>
2024-06-29 20:20:25 +0300

    Исправлено

--- src/Main.java
+++ src/Main.java
@@ -18,10 +18,14 @@ public class Main {
     }
 
     public static void isLeapYear(int year) {
-        if ((year % 4 != 0) && (year % 100 = 0) && (year % 400 != 0) && (year > 1584)) {
-            System.out.println(year + "год - не является високосный");
+        if ((year % 4 != 0) && (year > 1584)) {
+            System.out.println(year + " год не является високосным");
+        } else if (year % 100 != 0) {
+            System.out.println(year + " год является високосным");
+        } else if (year % 400 != 0) {
+            System.out.println(year + " год не является високосным");
         } else {
-            System.out.println(year + "год - високосный");
+            System.out.println(year + "год является високосным");
         }
     }
 
